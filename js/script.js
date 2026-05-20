b0ec076caeed5bcb5217ceb5b120bdd2382add28
Dic-ui <dicsas2150@gmail.com>
2026-05-20 10:27:43 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+```javascript
+console.log("Mesa de Servicios TI cargada correctamente");
+
+window.onload = function(){
+    alert("Bienvenido al Centro de Servicios TI de TecnoColombia S.A.S.");
+}
+```
